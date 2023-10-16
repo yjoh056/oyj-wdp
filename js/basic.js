3e8e64e6c106d01144f5e7fbf0bdd1947a46fe60
yjoh056 <yjoh056@gmail.com>
2023-10-16 13:16:13 +0900

    animate1

---
+++ js/basic.js
@@ -0,0 +1,48 @@
+var
+    words = ['디자인으로', '세상을 빛내다'],
+    part,
+    i = 0,
+    offset = 0,
+    len = words.length,
+    forwards = true,
+    skip_count = 0,
+    skip_delay = 5,
+    speed = 150;
+
+var wordflick = function () {
+    setInterval(function () {
+        if (forwards) {
+            if (offset >= words[i].length) {
+                ++skip_count;
+                if (skip_count == skip_delay) {
+                    forwards = false;
+                    skip_count = 0;
+                }
+            }
+        }
+        else {
+            if (offset == 0) {
+                forwards = true;
+                i++;
+                offset = 0;
+                if (i >= len) {
+                    i = 0;
+                }
+            }
+        }
+        part = words[i].substr(0, offset);
+        if (skip_count == 0) {
+            if (forwards) {
+                offset++;
+            }
+            else {
+                offset--;
+            }
+        }
+        $('.word').text(part);
+    }, speed);
+};
+
+$(document).ready(function () {
+    wordflick();
+});
