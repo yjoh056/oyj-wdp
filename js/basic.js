fb37715873bc2971ba5497790e2afdd4ebf114dd
yjoh056 <yjoh056@gmail.com>
2023-10-18 15:09:26 +0900

    main

--- js/basic.js
+++ js/basic.js
@@ -1,48 +1,28 @@
-var
-    words = ['디자인으로', '세상을 빛내다'],
-    part,
-    i = 0,
-    offset = 0,
-    len = words.length,
-    forwards = true,
-    skip_count = 0,
-    skip_delay = 5,
-    speed = 150;
-
-var wordflick = function () {
-    setInterval(function () {
-        if (forwards) {
-            if (offset >= words[i].length) {
-                ++skip_count;
-                if (skip_count == skip_delay) {
-                    forwards = false;
-                    skip_count = 0;
-                }
-            }
-        }
-        else {
-            if (offset == 0) {
-                forwards = true;
-                i++;
-                offset = 0;
-                if (i >= len) {
-                    i = 0;
-                }
-            }
-        }
-        part = words[i].substr(0, offset);
-        if (skip_count == 0) {
-            if (forwards) {
-                offset++;
-            }
-            else {
-                offset--;
-            }
-        }
-        $('.word').text(part);
-    }, speed);
-};
-
 $(document).ready(function () {
-    wordflick();
+  let mouseX, mouseY;
+  const ww = $(window).width();
+  const wh = $(window).height();
+  let traX, traY;
+  const i1InitialTop = parseFloat($(".i1").css("top"));
+  const i1InitialLeft = parseFloat($(".i1").css("left"));
+  const i2InitialTop = parseFloat($(".i2").css("top"));
+  const i2InitialLeft = parseFloat($(".i2").css("left"));
+
+  $(document).mousemove(function (e) {
+    mouseX = e.pageX;
+    mouseY = e.pageY;
+    traX = ((4 * mouseX) / 570) + 30;
+    traY = ((4 * mouseY) / 570) + 40;
+    // console.log(traX);
+    $(".title").css({ "background-position": traX + "%" + traY + "%" });
+    newX = ((2 * mouseX) / ww) - 0.5;
+    newY = ((2 * mouseY) / wh) - 0.5;
+    const newTop = i1InitialTop - (-newY * 80);
+    const newLeft = i1InitialLeft - (newX * 10);
+    const newTop2 = i2InitialTop - (newY * 90);
+    const newLeft2 = i2InitialLeft - (newX * 5);
+    $(".i1").css({ "top": newTop + "px", "left": newLeft + "px" });
+    $(".i2").css({ "top": newTop2 + "px", "left": -newLeft2 + "px" });
+  });
 });
+
