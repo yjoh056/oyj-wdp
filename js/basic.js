f7d62c75088e9dd642f104f6a8c5c6b3a7ddfee6
yjoh056 <yjoh056@gmail.com>
2023-11-24 13:47:05 +0900

    스크롤액션수정

--- js/basic.js
+++ js/basic.js
@@ -1,9 +1,7 @@
-//로딩
-$(window).on('load', function() {
-   $('.loading-overlay').fadeOut('slow');
+$(window).on('load', function () {
+  $('.loading-overlay').fadeOut('slow');
 });
 
-//cusor
 $(document).on('mousemove', function (e) {
   $('.custom-cursor').css({
     top: e.clientY,
@@ -11,48 +9,39 @@ $(document).on('mousemove', function (e) {
   });
 });
 
-//frame
 function changeClipPath() {
-  const frame = $('.main-photo-frame');  
+  const frame = $('.main-photo-frame');
   frame.addClass('transition');
-  setInterval(function(){
-      setTimeout(function () {
-    frame.css('clip-path', 'circle(50% at 50% 50%)');
-   },1000);
+  setInterval(function () {
+    setTimeout(function () {
+      frame.css('clip-path', 'circle(50% at 50% 50%)');
+    }, 1000);
   });
 }
 changeClipPath();
 
-//animation
 const animation = bodymovin.loadAnimation({
-  container: document.getElementById('lottie'), // Required
-  path: 'logo.json', // Required
-  renderer: 'svg', // Required
-  loop: true, // Optional
-  autoplay: true, // Optional
-})
-const scrollAnimation = bodymovin.loadAnimation({
-  container: document.getElementById('msc'), // Required
-  path: 'scroll.json', // Required
-  renderer: 'svg', // Required
-  loop: true, // Optional
-  autoplay: true, // Optional
-})
+  container: document.getElementById('lottie'),
+  path: 'logo.json',
+  renderer: 'svg',
+  loop: true,
+  autoplay: true
+});
 
-//
-//title effect
-//
+const scrollAnimation = bodymovin.loadAnimation({
+  container: document.getElementById('msc'),
+  path: 'scroll.json',
+  renderer: 'svg',
+  loop: true,
+  autoplay: true
+});
+
 $(document).ready(function () {
   let mouseX, mouseY;
   const ww = $(window).width();
   const wh = $(window).height();
   let traX, traY;
-  const i1InitialTop = parseFloat($(".i1").css("top"));
-  const i1InitialRight = parseFloat($(".i1").css("right"));
-  const i2InitialTop = parseFloat($(".i2").css("top"));
-  const i2InitialLeft = parseFloat($(".i2").css("left"));
 
-  //photo icon
   $(document).mousemove(function (e) {
     mouseX = e.pageX;
     mouseY = e.pageY;
@@ -61,25 +50,19 @@ $(document).ready(function () {
     $(".title").css({ "background-position": traX + "%" + traY + "%" });
     newX = ((2 * mouseX) / ww) - 0.5;
     newY = ((2 * mouseY) / wh) - 0.5;
-    const newTop = i1InitialTop - (newY * 50);
-    const newRight = i1InitialRight - (newX * 5);
-    const newTop2 = i2InitialTop - (-newY * 50);
-    const newLeft = i2InitialLeft - (newX * 5);
-    $(".i1").css({ "top": newTop + "px", "right": newRight + "px" });
-    $(".i2").css({ "top": newTop2 + "px", "left": newLeft + "px" });
+    $(".i1").css({ "top": i1InitialTop - (newY * 50) + "px", "right": i1InitialRight - (newX * 5) + "px" });
+    $(".i2").css({ "top": i2InitialTop - (-newY * 50) + "px", "left": i2InitialLeft - (newX * 5) + "px" });
   });
 });
+
 const mainInf = $('.main-infor li');
 mainInf.on('mouseenter', function () {
   mainInf.removeClass('on');
   $(this).addClass('on');
-})
+});
 
-//
-//scroll section(nev/gnb)
-//
 const win = $(window);
-const speed = Math.floor(win.height() * 0.9);
+const speed = Math.floor(win.height() * 1);
 const gnb = $('.gnb li');
 const sections = $('section');
 const sideNav = $('.sideNav>li');
@@ -89,37 +72,34 @@ let isAnimation = false;
 let winSCT;
 let topArr = [];
 
-
 function scrollToSection(index) {
   let section = sections.eq(index);
   let offset = section.offset().top;
   $('html,body').stop().animate({ scrollTop: offset }, 1000, 'easeOutCirc');
 }
+
 sections.each(function (i, section) {
   setTimeout(() => {
-      $(section).addClass(`bg${i + 1}`);
+    $(section).addClass(`bg${i + 1}`);
   }, i * 1000);
   const sectionTop = $(this).offset().top;
   topArr.push(sectionTop);
 });
 
-gnb.on({
-  click: function (e) {
-    e.preventDefault();
-    let index = $(this).index();
-    scrollToSection(index);
-    progressAnimaition();
-    pipScroll();
-  },
+gnb.on('click', function (e) {
+  e.preventDefault();
+  let index = $(this).index();
+  scrollToSection(index);
+  progressAnimaition();
+  pipScroll();
 });
-sideNav.on({
-  click: function (e) {
-    e.preventDefault();
-    let index = $(this).index();
-    scrollToSection(index);
-    progressAnimaition();
-    pipScroll();
-  },
+
+sideNav.on('click', function (e) {
+  e.preventDefault();
+  let index = $(this).index();
+  scrollToSection(index);
+  progressAnimaition();
+  pipScroll();
 });
 
 topBtn.stop().fadeOut();
@@ -131,11 +111,10 @@ topBtn.on('click', function (e) {
   });
 });
 
-
 win.on('scroll', function () {
   winSCT = win.scrollTop();
   sections.each(function (i) {
-    if (winSCT >= sections.eq(i).offset().top - 300) {
+    if (winSCT >= sections.eq(i).offset().top - speed) {
       gnb.removeClass('on');
       gnb.eq(i).addClass('on').siblings().removeClass('on');
       sideNav.eq(i).addClass('on').siblings().removeClass('on');
@@ -143,7 +122,7 @@ win.on('scroll', function () {
       if (winSCT > topArr[1] - speed) {
         topBtn.stop().fadeIn();
       }
-      if (winSCT > topArr[2] - speed && !isAnimation) {
+      if (winSCT > topArr[2] - (speed + 20) && !isAnimation) {
         progressAnimaition();
         isAnimation = true;
         pipScroll();
@@ -152,23 +131,15 @@ win.on('scroll', function () {
         if (!sections.eq(3).hasClass('is-animated')) {
           sections.eq(3).addClass('is-animated');
         }
-        
       }
-
     }
-
   });
 
   winSCT > 800 ? $('nav').addClass('sticky') : $('nav').removeClass('sticky');
-
 });
 
-
-// skill scroll
 function progressAnimaition() {
-
   pogressWrap.each(function () {
-
     const $this = $(this);
     const progressBar = $this.find('.bar');
     const progressText = $this.find('.rate');
@@ -194,44 +165,9 @@ function progressAnimaition() {
         }
       }
     );
-
   });
 }
 
-//
-// resume
-//
-const resumeLis = $('.resume-list-item');
-const resumeBtns = $('.resume-kind, .ex-list-table-btn'); // .
-const resumeTable = $('.resume-list-table');
-let resumeBtn = [];
-
-const exTable = $('.ex-list-table');
-resumeTable.hide();
-exTable.hide();
-
-// 첫 번째 테이블을 열린 상태로 초기화
-const firstTable = resumeLis.first().find('.resume-list-table, .ex-list-table');
-firstTable.show();
-
-// 클릭 이벤트 핸들러
-resumeBtns.on('click', function () {
-  const listItem = $(this).closest('.resume-list-item');
-  const table = listItem.find('.resume-list-table, .ex-list-table');
-  const resumeKind = listItem.find('.resume-kind');
-  listItem.toggleClass('active');
-  table.toggle();
-  if (listItem.hasClass('active')) {
-    resumeKind.css('font-weight', '700'); // 활성화된 상태에서는 글꼴 두께를 700으로 설정
-  } else {
-    resumeKind.css('font-weight', ''); // 비활성화된 상태에서는 글꼴 두께를 초기화
-  }
-});
-
-
-//
-//project
-//
 function pipScroll(param) {
   const boxes = $('.project-frame');
   boxes.each(function () {
@@ -240,7 +176,7 @@ function pipScroll(param) {
     const screen = device.find('.screen');
     const mask = device.find('.mask');
     const hightDifference = screen.innerHeight() - mask.innerHeight();
-// console.log(screen.innerHeight);
+
     box.on({
       mouseenter: function () {
         screen.stop().animate({ top: -hightDifference }, 2000);
@@ -256,10 +192,6 @@ win.on('resize', function () {
   pipScroll();
 });
 
-//
-//portfolio
-//
-
 const pfBtns = $('.portfolio-box-filter-links').find('button');
 pfBtns.on('click', function () {
   pfBtns.removeClass('active');
@@ -267,12 +199,9 @@ pfBtns.on('click', function () {
 });
 
 function filterImages(category) {
-  const grid = document.querySelector('.portfolio-box-work');
   const gridItems = document.querySelectorAll('.work-item');
-
   gridItems.forEach(function (item) {
     const itemCategory = item.getAttribute('data-category');
-
     if (category === 'all' || category === itemCategory) {
       item.style.display = 'block';
     } else {
@@ -283,24 +212,48 @@ function filterImages(category) {
 
 function showAllImages() {
   const gridItems = document.querySelectorAll('.work-item');
-
   gridItems.forEach(function (item) {
     item.style.display = 'block';
   });
 }
 
-const pics=$(".pic");
-const lightbox=$("#lightbox");
-const lightImg=$("#lightImage");
+const pics = $(".pic");
+const lightbox = $("#lightbox");
+const lightImg = $("#lightImage");
 
-pics.on('click',function(){
-    const bigLocation=$(this).attr("data-src");
-    lightImg.load(bigLocation);
-    lightbox.css('display', 'block');
-    $('html').addClass('all_scrollFixed');
-})
+pics.on('click', function () {
+  const bigLocation = $(this).attr("data-src");
+  lightImg.load(bigLocation);
+  lightbox.css('display', 'block');
+  $('html').addClass('all_scrollFixed');
+});
 
-lightbox.on('click',function(){
-    lightbox.css('display', 'none');
-    $('html').removeClass('all_scrollFixed');
-})
+lightbox.on('click', function () {
+  lightbox.css('display', 'none');
+  $('html').removeClass('all_scrollFixed');
+});
+
+const resumeLis = $('.resume-list-item');
+const resumeBtns = $('.resume-kind, .ex-list-table-btn');
+const resumeTable = $('.resume-list-table');
+let resumeBtn = [];
+
+const exTable = $('.ex-list-table');
+resumeTable.hide();
+exTable.hide();
+
+const firstTable = resumeLis.first().find('.resume-list-table, .ex-list-table');
+firstTable.show();
+
+resumeBtns.on('click', function () {
+  const listItem = $(this).closest('.resume-list-item');
+  const table = listItem.find('.resume-list-table, .ex-list-table');
+  const resumeKind = listItem.find('.resume-kind');
+  listItem.toggleClass('active');
+  table.toggle();
+  if (listItem.hasClass('active')) {
+    resumeKind.css('font-weight', '700');
+  } else {
+    resumeKind.css('font-weight', '');
+  }
+});
